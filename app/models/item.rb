1f88b0552a1d91f9d3e2e3fa9d6b360c3206e006
Dan Oliphant <danoliphant@mac.com>
2015-03-31 19:46:30 -0500

    Days left update

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,30 @@ class Item < ActiveRecord::Base
 
   scope :default_order, -> () { order('items.created_at ASC') }
 
-  def days_left
-    6 - (DateTime.now.to_date - created_at.to_date).to_i
+  def days_to_complete
+    7.days
   end
 
+  def due_date
+    created_at.to_date + days_to_complete
+  end
+
+
+  def days_left
+    (due_date - DateTime.now.to_date).to_i
+  end
+
+end
+
+module ItemsHelper
+
+  def remaining_time(days_left)
+    if days_left > 1
+      "#{days_left} days"
+    elsif days_left == 0
+      "Due Today!"
+    else
+      "Overdue!!"
+    end
+  end
 end
